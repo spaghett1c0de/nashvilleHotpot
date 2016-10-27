ab536a319d84ed5a7ea37f81cb6f3db923859352
vincentliu130 <she4869@hotmail.com>
2016-10-27 06:50:11 -0400

    minor change

--- assets/js/customScript.js
+++ assets/js/customScript.js
@@ -70,10 +70,10 @@
 
 			if($wrapper.css('marginLeft')=='0px'){
 				$drawerRight.animate({marginRight : 0},200);
-				$wrapper.animate({marginLeft : -300},200);
+				$wrapper.animate({marginLeft : -210},200);
 			}
 			else{
-				$drawerRight.animate({marginRight : -300},200);
+				$drawerRight.animate({marginRight : -210},200);
 				$wrapper.animate({marginLeft : 0},200);
 			}
 			
